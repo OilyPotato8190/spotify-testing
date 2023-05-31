e9cbcf069a72b6039007434617880ff1002fe7a9
OilyPotato819 <o.schneider@share.epsb.ca>
2023-05-30 23:11:45 -0600

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,81 @@
+async function getToken() {
+  const response = await fetch('/getToken');
+  const data = await response.text();
+  return data;
+}
+
+async function getQueue() {
+  const queueRes = await fetch('https://api.spotify.com/v1/me/player/queue', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return JSON.parse(await queueRes.text());
+}
+
+let token = getToken();
+
+window.onSpotifyWebPlaybackSDKReady = async () => {
+  token = await token;
+  const player = new Spotify.Player({
+    name: 'Web Playback SDK Quick Start Player',
+    getOAuthToken: (cb) => {
+      cb(token);
+    },
+    volume: 0.5,
+  });
+
+  // Ready
+  player.addListener('ready', ({ device_id }) => {
+    console.log('Ready with Device ID', device_id);
+    fetch('https://api.spotify.com/v1/me/player', {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        device_ids: [device_id],
+        play: true,
+      }),
+    });
+
+    document.getElementById('test').onclick = async function () {
+      const query = 'david';
+      const type = 'track';
+      const limit = 5;
+      const results = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}&limit=${limit}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const text = JSON.parse(await results.text());
+      const uri = text.tracks.items[0];
+    };
+  });
+
+  // Not Ready
+  player.addListener('not_ready', ({ device_id }) => {
+    console.log('Device ID has gone offline', device_id);
+  });
+
+  player.addListener('initialization_error', ({ message }) => {
+    console.error(message);
+  });
+
+  player.addListener('authentication_error', ({ message }) => {
+    console.error(message);
+  });
+
+  player.addListener('account_error', ({ message }) => {
+    console.error(message);
+  });
+
+  document.getElementById('togglePlay').onclick = function () {
+    player.togglePlay();
+  };
+
+  player.connect();
+};
