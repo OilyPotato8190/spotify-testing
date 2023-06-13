3156fc6e6270e3b49330899515f28a0a64aa9b21
OilyPotato8190 <o.schneider@share.epsb.ca>
2023-06-13 14:47:13 -0600

    invalid token scopes

--- main.py
+++
@@ -1,4 +0,0 @@
-import requests
-response = requests.put(
-    url="http://localhost:3000/sendQuery", data="query", headers={'Content-Type': 'text/plain'})
-# put query
--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
 async function getToken() {
-  const response = await fetch("/getToken");
+  const response = await fetch('/getToken');
   const data = await response.text();
   return data;
 }
 
 async function getQueue() {
-  const queueRes = await fetch("https://api.spotify.com/v1/me/player/queue", {
-    method: "GET",
+  const queueRes = await fetch('https://api.spotify.com/v1/me/player/queue', {
+    method: 'GET',
     headers: {
       Authorization: `Bearer ${token}`,
     },
@@ -19,7 +19,7 @@ let token = getToken();
 window.onSpotifyWebPlaybackSDKReady = async () => {
   token = await token;
   const player = new Spotify.Player({
-    name: "Web Playback SDK Quick Start Player",
+    name: 'Web Playback SDK Quick Start Player',
     getOAuthToken: (cb) => {
       cb(token);
     },
@@ -27,42 +27,39 @@ window.onSpotifyWebPlaybackSDKReady = async () => {
   });
 
   // Ready
-  player.addListener("ready", ({ device_id }) => {
-    console.log("Ready with Device ID", device_id);
-    fetch("https://api.spotify.com/v1/me/player", {
-      method: "PUT",
+  player.addListener('ready', ({ device_id }) => {
+    console.log('Ready with Device ID', device_id);
+    fetch('https://api.spotify.com/v1/me/player', {
+      method: 'PUT',
       headers: {
         Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         device_ids: [device_id],
       }),
-    }).then(() => console.log("transfered playback"));
+    }).then(() => console.log('transfered playback'));
 
-    document.getElementById("choose").onclick = async function () {
-      const query = document.getElementById("song-input").value;
+    document.getElementById('choose').onclick = async function () {
+      const query = document.getElementById('song-input').value;
       if (!query) return;
-      const type = "track";
+      const type = 'track';
       const limit = 1;
 
-      const results = await fetch(
-        `https://api.spotify.com/v1/search?q=${query}&type=${type}&limit=${limit}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      const results = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}&limit=${limit}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       const text = JSON.parse(await results.text());
       const track = text.tracks.items[0];
 
-      await fetch("https://api.spotify.com/v1/me/player/play", {
-        method: "PUT",
+      await fetch('https://api.spotify.com/v1/me/player/play', {
+        method: 'PUT',
         headers: {
           Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           uris: [track.uri],
@@ -70,36 +67,31 @@ window.onSpotifyWebPlaybackSDKReady = async () => {
       }).then(() => console.log(`chose song ${track.name}`));
     };
 
-    document.getElementById("queue").onclick = async function () {
+    document.getElementById('queue').onclick = async function () {
       console.log((await getQueue()).queue.map((x) => x.name));
     };
   });
 
   // Not Ready
-  player.addListener("not_ready", ({ device_id }) => {
-    console.log("Device ID has gone offline", device_id);
+  player.addListener('not_ready', ({ device_id }) => {
+    console.log('Device ID has gone offline', device_id);
   });
 
-  player.addListener("initialization_error", ({ message }) => {
+  player.addListener('initialization_error', ({ message }) => {
     console.error(message);
   });
 
-  player.addListener("authentication_error", ({ message }) => {
+  player.addListener('authentication_error', ({ message }) => {
     console.error(message);
   });
 
-  player.addListener("account_error", ({ message }) => {
+  player.addListener('account_error', ({ message }) => {
     console.error(message);
   });
 
-  document.getElementById("togglePlay").onclick = async function () {
+  document.getElementById('togglePlay').onclick = async function () {
     player.togglePlay();
   };
 
   player.connect();
 };
-
-fetch("http://localhost:3000/sendQuery", {
-  method: "PUT",
-  body: "query bla bla bla",
-});
